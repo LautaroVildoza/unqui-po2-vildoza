07c5481a5266615bc53baa57f9c129dcaf756366
Lautaro Vildoza <lautarovildoza1@gmail.com>
2025-05-25 17:05:43 -0300

    Agregando Estructura en el tp 10

---
+++ src/ar/edu/unq/po2/tp10/Ejercicio0.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.po2.tp10;
+
+public class Ejercicio0 {
+/**
+ * 
+ * Forma 1:
+ * 		Ventajas:
+ * 
+ * 		Desventajas:
+ * 
+ * 
+ * 
+ * Forma 2:
+ * 
+ * 		Ventajas:
+ * 
+ * 
+ * 		Desventajas:
+ * 
+ * 
+ * Forma 3:
+ * 
+ * 		Ventajas:
+ * 
+ * 
+ * 		Desventajas:
+ * 
+ */
+}
